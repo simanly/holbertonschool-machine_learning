c882f44ceff151ca135ec3e9114e8d5ba7529b65
Sofiya <imanlysofi@gmail.com>
2026-04-18 14:30:58 +0000

    Fix

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,15 +5,15 @@ This module provides a function that uses a mathematical formula
 to achieve O(1) time complexity.
 '''
 def summation_i_squared(n):
-'''
-Calculates the sum of squares from 1 to n.
+    '''
+    Calculates the sum of squares from 1 to n.
 
-    Args:
-        n (int): The stop value for the sum.
+        Args:
+            n (int): The stop value for the sum.
 
-    Returns:
-        int: The sum of squares if n is a valid positive integer, 0 otherwise.
-'''
+        Returns:
+            int: The sum of squares if n is a valid positive integer, 0 otherwise.
+    '''
     if not isinstance(n, int) or n < 1:
         return 0
     return (n*(n+1)*(2*n+1)) // 6
