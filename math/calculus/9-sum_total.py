1233544aab6020b0040275d433e92ec921d56c10
Sofiya <imanlysofi@gmail.com>
2026-04-18 14:35:20 +0000

    Fixx

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -14,6 +14,6 @@ def summation_i_squared(n):
         Returns:
             int: The sum of squares if n is a valid positive integer, 0 otherwise.
     '''
-    if not isinstance(n, int) or n < 1:
+    if not isinstance(n, int):
         return 0
-    return (n*(n+1)*(2*n+1)) // 6
+    return (n * (n + 1) * (2 * n + 1)) // 6
