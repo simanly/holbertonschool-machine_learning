835910ac9170bcfafe6d9ee95d589f4199c785ef
Sofiya <imanlysofi@gmail.com>
2026-04-18 14:48:14 +0000

    fix again

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-"""
+'''
 Module to calculate the sum of squares of integers up to n.
 This module provides a function that uses a mathematical formula.
-"""
+'''
 
 
 def summation_i_squared(n):
-    """
+    '''
     Calculates the sum of squares from 1 to n.
-
     Args:
         n (int): The stop value for the sum.
-
     Returns:
         int: The sum of squares, or 0 if n is not valid.
-    """
+    '''
     if not isinstance(n, int) or n < 1:
-        return 0
+        return None
     return (n * (n + 1) * (2 * n + 1)) // 6
 
