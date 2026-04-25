2a8a198f5278ffcec2abc853287c5bc0f2dd498d
Sofiya <imanlysofi@gmail.com>
2026-04-25 13:03:21 +0000

    Task 11: transposes matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Defines a function that transposes matrix
+'''
+
+
+def np_transpose(matrix):
+    '''
+    Returns the transpose like new numpy array
+    '''
+    return matrix.transpose()
