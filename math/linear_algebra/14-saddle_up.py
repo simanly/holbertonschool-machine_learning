99c4e0585045789bc5daca6dee12376765c89dbd
Sofiya <imanlysofi@gmail.com>
2026-04-25 13:44:18 +0000

    Task 14: matrix multiplication with numpy

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Defines a function that performs matrix multiplication
+'''
+
+
+def np_matmul(mat1, mat2):
+    '''
+    Returns a prod of matrix
+    '''
+    return mat1 @ mat2
