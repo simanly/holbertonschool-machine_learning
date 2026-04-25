ea569a15cee51351a6171d35f3fc78be675b0fad
Sofiya <imanlysofi@gmail.com>
2026-04-25 12:47:39 +0000

    Task 10: calculates the shape of Numpy Array

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Defines a function that calculates the shape of numpy array
+'''
+
+
+def np_shape(matrix):
+    '''
+    Return the shape of numpy array
+    '''
+    return matrix.shape
