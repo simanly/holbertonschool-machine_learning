6e49a7e879e1ea2ba8412dbc9354156b37702969
Sofiya <imanlysofi@gmail.com>
2026-05-09 19:14:38 +0000

    Task 0: poisson distributio

---
+++ math/probability/poisson.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+Contains the Poisson class representing a poisson distribution
+'''
+
+class Poisson:
+    '''
+    Class that represents a poisson distribution
+    '''
+
+    def __init__(self, data=None, lambtha=1.):
+        '''
+        Initialize Poisson class
+        Args:
+            data: list of the data to be used to estimate the distribution
+            lambtha: expected number of occurrences in a given time frame
+        '''
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = float(sum(data) / len(data))
+
