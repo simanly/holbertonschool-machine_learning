627ad8ba93479609bce21cdf85405994d4998113
Sofiya <imanlysofi@gmail.com>
2026-05-09 19:53:43 +0000

    Task 5: update the class exponential: CDF

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -34,3 +34,13 @@ class Exponential:
         e = 2.7182818285
 
         return self.lambtha * (e ** (-self.lambtha * x))
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period
+        """
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+        return 1 - (e ** (-self.lambtha * x))
