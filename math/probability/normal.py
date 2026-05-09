3033373778d83910c1a2b3b60c4b1a2bfbdfeb3f
Sofiya <imanlysofi@gmail.com>
2026-05-09 19:57:53 +0000

    udate

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -30,3 +30,15 @@ class Normal:
             variance = sum_diff_sq / len(data)
 
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score
+        """
+        return (z * self.stddev) + self.mean
