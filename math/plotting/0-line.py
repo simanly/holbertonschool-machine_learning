94a8ee31ee05b369da8923e718e2015b901e681b
Sofiya <imanlysofi@gmail.com>
2026-05-29 23:03:25 +0000

    Task 0: Line Graph

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    x = np.arange(0, 11)
+    plt.plot(x, y, color='red', linestyle='solid')
+    plt.xlim(0, 10)
+    plt.show()
