a8f361b8cee57d758c4f9783a5755ae9a9fdcb1b
Sofiya <imanlysofi@gmail.com>
2026-05-29 23:20:02 +0000

    Add documentattion

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+'''
+Module to plot a cubic line graph
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    '''
+    Plots y as a solid red line with x-axis from 0 to 10
+    '''
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
