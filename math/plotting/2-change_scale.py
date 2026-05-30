ef682f856ca5960528ac070787abd04f5e5d1415
Sofiya <imanlysofi@gmail.com>
2026-05-30 01:21:50 +0000

    Task 2: Change of scale

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+Module to plot the exponential decay of Carbon-14
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    '''
+    Plots a line graph with a logarithmic y-axis scale
+    '''
+
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y)
+    plt.yscale('log')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
+    plt.xlim(0, 28650)
+    plt.show()
+
