964d1f85e83aec371edd562b79abe9100f393872
Sofiya <imanlysofi@gmail.com>
2026-05-30 00:45:48 +0000

    Task 1: Scatter

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+Module to plot a scatter plot of men's height vs weight
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    '''
+    Plots a scatter plot with specific labels, title, and color
+    '''
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.scatter(x, y, color='magenta')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
