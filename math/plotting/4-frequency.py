0f41627120cba15283de87d9769b79b79e0e5bf8
Sofiya <imanlysofi@gmail.com>
2026-05-30 02:01:38 +0000

    Task 4: Frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+Module to plot a histogram of student grades
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    '''
+    Plots a histogram with custom bins, labels, titles, and borders
+    '''
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.show()
