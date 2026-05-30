c3053b7cc23866bb5389f0fc9ab6792d036d8735
Sofiya <imanlysofi@gmail.com>
2026-05-30 02:10:08 +0000

    fixx

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,14 +10,16 @@ def frequency():
     '''
     Plots a histogram with custom bins, labels, titles, and borders
     '''
+
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
     bins = np.arange(0, 101, 10)
     plt.hist(student_grades, bins=bins, edgecolor='black')
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
     plt.xlim(0, 100)
     plt.ylim(0, 30)
+    plt.show()
