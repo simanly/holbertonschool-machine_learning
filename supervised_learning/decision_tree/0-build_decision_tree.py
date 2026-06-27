fae0535069db286f20504eb4b1bbc59953b027d8
Sofiya <imanlysofi@gmail.com>
2026-06-27 12:05:20 +0000

    Task 0: Depth of a decision tree

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Node:
     def __init__(self, feature=None, threshold=None, left_child=None,
-                right_child=None, is_root=False, depth=0):
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -38,7 +38,7 @@ class Leaf(Node):
 
 class Decision_Tree():
     def __init__(self, max_depth=10, min_pop=1, seed=0,
-                split_criterion="random", root=None):
+                 split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
