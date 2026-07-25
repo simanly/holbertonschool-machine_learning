fb9116a31f3178c352addef5c2bedcaac609a393
Sofiya <imanlysofi@gmail.com>
2026-07-25 17:07:55 +0000

    Task 3: One-hot encoding

---
+++ supervised_learning/keras/3-one-hot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module to convert a label vector into a one-hot matrix.
+"""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a label vector into a one-hot matrix.
+
+    Args:
+        labels: label vector to be converted
+        classes: maximum number of classes (optional)
+
+    Returns:
+        The one-hot matrix with classes as the last dimension
+    """
+    return K.utils.to_categorical(labels, num_classes=classes)
