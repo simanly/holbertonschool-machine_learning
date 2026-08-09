e281a0f36ce5a6ad58d61d1f555b9956cf47353e
Sofiya <imanlysofi@gmail.com>
2026-08-09 08:02:39 +0000

    Task 1: Normalize

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def normalize(X, m, s):
+    """
+    Normalizes (standardizes) a matrix X using mean and standard deviation.
+
+    Parameters:
+    - X: numpy.ndarray of shape (d, nx) to normalize
+    - m: numpy.ndarray of shape (nx,) containing the mean of all features
+    - s: numpy.ndarray of shape (nx,) containing the standard deviation of all features
+
+    Returns:
+    - Normalized numpy.ndarray of shape (d, nx)
+    """
+    return (X - m) / s
