ecc16aa221d3fa423050a7fca2ee882b84fbdd99
Sofiya <imanlysofi@gmail.com>
2026-08-30 22:47:07 +0000

    Task 2 fix

--- supervised_learning/regularization/0-l2_reg_cost.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Calculates the cost of a neural network with L2 regularization in Keras."""
-import tensorflow as tf
-
-
-def l2_reg_cost(cost, model):
-    """Calculates total cost for each layer accounting for L2 regularization."""
-    return cost + model.losses
--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-"""L2 Regularization Gradient Descent."""
-import numpy as np
+"""
+Calculates the cost of a neural network
+with L2 regularization in Keras.
+"""
+import tensorflow as tf
 
 
-def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Updates weights and biases with L2 regularization."""
-    m = Y.shape[1]
-    dZ = cache[f'A{L}'] - Y
-
-    for i in range(L, 0, -1):
-        A_prev = cache[f'A{i - 1}']
-
-        dW = (np.matmul(dZ, A_prev.T) / m) + ((lambtha / m) * weights[f'W{i}'])
-        db = np.sum(dZ, axis=1, keepdims=True) / m
-
-        if i > 1:
-            dZ = np.matmul(weights[f'W{i}'].T, dZ) * (1 - np.square(A_prev))
-
-        weights[f'W{i}'] -= alpha * dW
-        weights[f'b{i}'] -= alpha * db
+def l2_reg_cost(cost, model):
+    """
+    Calculates total cost for
+    each layer accounting for L2 regularization.
+    """
+    return cost + model.losses
