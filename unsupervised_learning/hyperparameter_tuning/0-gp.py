eac367357116fbe1fdb7137266456b0a9bd58b1c
Sofiya <imanlysofi@gmail.com>
2026-08-31 14:03:33 +0000

    fix validation

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -16,5 +16,6 @@ class GaussianProcess:
 
     def kernel(self, X1, X2):
         """Calculates the covariance kernel matrix using RBF."""
-        sqdist = np.sum(X1 ** 2, 1).reshape(-1, 1) + np.sum(X2 ** 2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = np.sum(X1 ** 2, 1).reshape(-1, 1) + np.sum(X2 ** 2, 1) - \
+            2 * np.dot(X1, X2.T)
         return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
