40956e585d8c569fa644dc490a2afba9ffa474a9
Sofiya <imanlysofi@gmail.com>
2026-08-31 19:31:48 +0000

    fix

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
-"""Module 0-rnn_cell"""
+"""
+Module 1-rnn
+Defines the function rnn that performs forward
+propagation for a simple RNN.
+"""
 import numpy as np
 
 
-class RNNCell:
-    """Class RNNCell"""
+def rnn(rnn_cell, X, h_0):
+    """
+    Performs forward propagation for a simple RNN.
 
-    def __init__(self, i, h, o):
-        self.Wh = np.random.randn(h + i, h)
-        self.Wy = np.random.randn(h, o)
-        self.bh = np.zeros((1, h))
-        self.by = np.zeros((1, o))
+    Parameters:
+    - rnn_cell: instance of RNNCell used for forward propagation
+    - X: numpy.ndarray of shape (t, m, i) containing data to be used
+    - h_0: numpy.ndarray of shape (m, h) containing initial hidden state
 
-    def forward(self, h_prev, x_t):
-        concat = np.concatenate((h_prev, x_t), axis=1)
-        h_next = np.tanh(np.matmul(concat, self.Wh) + self.bh)
-        y_logit = np.matmul(h_next, self.Wy) + self.by
-        e_x = np.exp(y_logit - np.max(y_logit, axis=1, keepdims=True))
-        y = e_x / np.sum(e_x, axis=1, keepdims=True)
-        return h_next, y
+    Returns:
+    - H: numpy.ndarray containing all hidden states, shape (t + 1, m, h)
+    - Y: numpy.ndarray containing all outputs, shape (t, m, o)
+    """
+    t, m, _ = X.shape
+    h = h_0.shape[1]
+    o = rnn_cell.Wy.shape[1]
+
+    H = np.zeros((t + 1, m, h))
+    H[0] = h_0
+    Y = np.zeros((t, m, o))
+
+    h_prev = h_0
+    for step in range(t):
+        h_prev, y_t = rnn_cell.forward(h_prev, X[step])
+        H[step + 1] = h_prev
+        Y[step] = y_t
+
+    return H, Y
