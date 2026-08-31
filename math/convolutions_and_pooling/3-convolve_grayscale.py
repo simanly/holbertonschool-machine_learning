d29cc9d03c1c65cb41b6672df2315257fa8a5081
Sofiya <imanlysofi@gmail.com>
2026-08-31 12:11:59 +0000

    Task 3: Strided Convolution

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Strided convolution on grayscale images."""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """Performs a convolution on grayscale images."""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    images_padded = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw)),
+        mode='constant',
+        constant_values=0
+    )
+
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    convolved = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            slice_images = images_padded[
+                :,
+                i * sh:i * sh + kh,
+                j * sw:j * sw + kw
+            ]
+            convolved[:, i, j] = np.sum(slice_images * kernel, axis=(1, 2))
+
+    return convolved
