0533073c6449dcd206f19780e4086ee8341557d1
Sofiya <imanlysofi@gmail.com>
2026-08-31 13:38:13 +0000

    Task 5: Multiple Kernels

---
+++ math/convolutions_and_pooling/5-convolve.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Convolution on images using multiple kernels."""
+import numpy as np
+
+
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    """Performs a convolution on images using multiple kernels."""
+    m, h, w, c = images.shape
+    kh, kw, _, nc = kernels.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    images_padded = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode='constant',
+        constant_values=0
+    )
+
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    convolved = np.zeros((m, output_h, output_w, nc))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            for k in range(nc):
+                slice_images = images_padded[
+                    :,
+                    i * sh:i * sh + kh,
+                    j * sw:j * sw + kw,
+                    :
+                ]
+                kernel = kernels[:, :, :, k]
+                convolved[:, i, j, k] = np.sum(
+                    slice_images * kernel,
+                    axis=(1, 2, 3)
+                )
+
+    return convolved
