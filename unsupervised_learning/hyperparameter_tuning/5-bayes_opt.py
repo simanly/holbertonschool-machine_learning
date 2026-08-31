1049fb584cc03194e4f8be4f509f5438074fddc2
Sofiya <imanlysofi@gmail.com>
2026-08-31 14:39:11 +0000

    Task 5: Bayesian Optimization

---
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""BayesianOptimization class optimize method."""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """Performs Bayesian optimization on a noiseless 1D Gaussian process."""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """Initializes the BayesianOptimization class."""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        min_val, max_val = bounds
+        self.X_s = np.linspace(min_val, max_val, ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """Calculates the next best sample location using EI."""
+        mu, sigma = self.gp.predict(self.X_s)
+
+        if self.minimize:
+            y_opt = np.min(self.gp.Y)
+            improvement = y_opt - mu - self.xsi
+        else:
+            y_opt = np.max(self.gp.Y)
+            improvement = mu - y_opt - self.xsi
+
+        with np.errstate(divide='ignore'):
+            Z = improvement / sigma
+            EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+            EI[sigma == 0.0] = 0.0
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, EI
+
+    def optimize(self, iterations=100):
+        """Optimizes the black-box function.
+
+        iterations: maximum number of iterations to perform
+
+        Returns: x_opt, Y_opt
+            x_opt: numpy.ndarray (1,) representing the optimal point
+            Y_opt: numpy.ndarray (1,) representing optimal function value
+        """
+        for _ in range(iterations):
+            x_next, _ = self.acquisition()
+
+            if np.any(np.isclose(x_next, self.gp.X)):
+                break
+
+            y_next = self.f(x_next)
+            self.gp.update(x_next, y_next)
+
+        if self.minimize:
+            opt_idx = np.argmin(self.gp.Y)
+        else:
+            opt_idx = np.argmax(self.gp.Y)
+
+        x_opt = self.gp.X[opt_idx]
+        Y_opt = self.gp.Y[opt_idx]
+
+        return x_opt, Y_opt
