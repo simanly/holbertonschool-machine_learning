fcdecbcf0254731273a87f3c5d0e06b0ee0586af
Sofiya <imanlysofi@gmail.com>
2026-09-02 14:31:14 +0000

    Task 1: Crop

---
+++ pipeline/data_augmentation/1-crop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Random image cropping module."""
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """
+    Performs a random crop of an image.
+
+    Parameters:
+    - image: 3D tf.Tensor
+    - size: tuple containing the size of the crop
+
+    Returns:
+    - cropped image
+    """
+    return tf.image.random_crop(image, size=size)
