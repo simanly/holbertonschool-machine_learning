ae925265e06216ee68ca613ffc508fc11813b456
simanly <imanlysofi@gmail.com>
2026-09-04 18:23:18 +0400

    Fix validation

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -9,7 +9,8 @@ def change_brightness(image, max_delta):
 
     Parameters:
     - image: 3D tf.Tensor containing the image to change
-    - max_delta: float, maximum amount the image should be brightened or darkened
+    - max_delta: float, maximum amount the image should be
+    brightened or darkened
 
     Returns:
     - The altered image
