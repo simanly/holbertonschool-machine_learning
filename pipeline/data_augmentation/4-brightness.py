43bb6fd730a2322f897bf17bbb86904770aa0a0b
simanly <imanlysofi@gmail.com>
2026-09-04 18:18:12 +0400

    Task 4: Brightness
    This module provides a function to randomly adjust the brightness of images using TensorFlow.

---
+++ pipeline/data_augmentation/4-brightness.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Random brightness adjustment module."""
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """
+    Randomly changes the brightness of an image.
+
+    Parameters:
+    - image: 3D tf.Tensor containing the image to change
+    - max_delta: float, maximum amount the image should be brightened or darkened
+
+    Returns:
+    - The altered image
+    """
+    return tf.image.random_brightness(image, max_delta)
