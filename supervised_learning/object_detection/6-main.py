4c18c5e69cbc3125d09c8408a0d57f4149658933
Sofiya <imanlysofi@gmail.com>
2026-09-05 20:13:56 +0400

    Fix

--- supervised_learning/object_detection/6-main.py
+++ supervised_learning/object_detection/6-main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
                         [[30, 61], [62, 45], [59, 119]],
                         [[10, 13], [16, 30], [33, 23]]])
     yolo = Yolo('yolo.h5', 'coco_classes.txt', 0.6, 0.5, anchors)
-    images, image_paths = yolo.load_images('yolo_images/yolo/')
+    images, image_paths = yolo.load_images('yolo_images/')
     boxes = np.array([[119.22100287, 118.62197718, 567.75985556, 440.44121152],
                       [468.53530752, 84.48338278, 696.04923556, 167.98947829],
                       [124.2043716, 220.43365057, 319.4254314 , 542.13706101]])
