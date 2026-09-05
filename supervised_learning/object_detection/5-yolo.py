c2e258d8c470a26fda3bcb64801bd950b24103f7
Sofiya <imanlysofi@gmail.com>
2026-09-05 18:18:19 +0400

    fix

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -8,7 +8,17 @@ import tensorflow as tf
 class Yolo:
     """Contains methods for initializing and processing YOLO model data."""
 
-    # ... __init__, process_outputs, filter_boxes, non_max_suppression, load_images ...
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Initializes the Yolo class instance."""
+        self.model = tf.keras.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    # ... здесь должны быть остальные методы: process_outputs, filter_boxes,
+    # non_max_suppression, load_images ...
 
     def preprocess_images(self, images):
         """Resizes and rescales images for input into the YOLO model."""
