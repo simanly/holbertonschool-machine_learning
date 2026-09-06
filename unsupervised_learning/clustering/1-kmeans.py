855c67763045cc4ff6051f201ca403771ca7d243
Sofiya <imanlysofi@gmail.com>
2026-09-06 21:53:42 +0400

    fix

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """Performs K-means clustering on a dataset."""
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    """Initializes cluster centroids for K-means"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k <= 0 or k > X.shape[0]:
+        return None
+    low = np.min(X, axis=0)
+    high = np.max(X, axis=0)
+    return np.random.uniform(low, high, size=(k, X.shape[1]))
 
 
 def kmeans(X, k, iterations=1000):
@@ -11,14 +21,13 @@ def kmeans(X, k, iterations=1000):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if type(k) is not int or k <= 0:
+    if type(k) is not int or k <= 0 or k > X.shape[0]:
         return None, None
     if type(iterations) is not int or iterations <= 0:
         return None, None
 
     low = np.min(X, axis=0)
     high = np.max(X, axis=0)
-
     C = initialize(X, k)
     if C is None:
         return None, None
@@ -26,7 +35,9 @@ def kmeans(X, k, iterations=1000):
     for _ in range(iterations):
         C_prev = np.copy(C)
 
-        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        distances = np.zeros((X.shape[0], k))
+        for j in range(k):
+            distances[:, j] = np.linalg.norm(X - C[j], axis=1)
         clss = np.argmin(distances, axis=1)
 
         for j in range(k):
@@ -36,7 +47,9 @@ def kmeans(X, k, iterations=1000):
             else:
                 C[j] = np.mean(points, axis=0)
 
-        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        distances = np.zeros((X.shape[0], k))
+        for j in range(k):
+            distances[:, j] = np.linalg.norm(X - C[j], axis=1)
         clss = np.argmin(distances, axis=1)
 
         if (C == C_prev).all():
