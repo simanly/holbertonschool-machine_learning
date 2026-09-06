33c8ffd6e9b0976b560f60341691abd42acafabd
Sofiya <imanlysofi@gmail.com>
2026-09-06 14:31:45 +0400

    Fix Task 0

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""
-Contains the NST class for Neural Style Transfer
-"""
+"""Neural Style Transfer Module"""
+
 import numpy as np
 import tensorflow as tf
 
 
 class NST:
-    """
-    Class that performs tasks for Neural Style Transfer
-    """
+    """Class NST that performs Neural Style Transfer"""
 
     style_layers = [
         'block1_conv1',
@@ -21,25 +18,25 @@ class NST:
     content_layer = 'block4_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """
-        Class constructor for Neural Style Transfer
-        """
+        """Initializes the NST class instance"""
         if (not isinstance(style_image, np.ndarray)
-                or style_image.ndim != 3
-                or style_image.shape[2] != 3):
-            msg = 'style_image must be a numpy.ndarray with shape (h, w, 3)'
-            raise TypeError(msg)
+                or style_image.ndim != 3 or style_image.shape[2] != 3):
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
 
         if (not isinstance(content_image, np.ndarray)
-                or content_image.ndim != 3
-                or content_image.shape[2] != 3):
-            msg = 'content_image must be a numpy.ndarray with shape (h, w, 3)'
-            raise TypeError(msg)
+                or content_image.ndim != 3 or content_image.shape[2] != 3):
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
 
-        if not isinstance(alpha, (int, float)) or alpha < 0:
+        if not isinstance(alpha, (int, float)) or isinstance(alpha, bool) \
+                or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
 
-        if not isinstance(beta, (int, float)) or beta < 0:
+        if not isinstance(beta, (int, float)) or isinstance(beta, bool) \
+                or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
         self.style_image = self.scale_image(style_image)
@@ -48,31 +45,31 @@ class NST:
         self.beta = beta
 
     @staticmethod
-    def scale_image(image):
-        """
-        Rescales an image such that its pixels values are between 0 and 1
-        and its largest side is 512 pixels
-        """
+    def scale_image(image, max_dim=512):
+        """Rescales an image so that its maximum dimension is max_dim"""
         if (not isinstance(image, np.ndarray)
-            or image.ndim != 3
-                or image.shape[2] != 3):
-            msg = 'image must be a numpy.ndarray with shape (h, w, 3)'
-            raise TypeError(msg)
+                or image.ndim != 3 or image.shape[2] != 3):
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        if type(max_dim) is not int or max_dim <= 0:
+            raise TypeError("max_dim must be a positive integer")
 
         h, w, _ = image.shape
-        max_dim = max(h, w)
-        scale = 512 / max_dim
+        if h > w:
+            h_new = max_dim
+            w_new = int((w * max_dim) / h)
+        else:
+            w_new = max_dim
+            h_new = int((h * max_dim) / w)
 
-        h_new = int(round(h * scale))
-        w_new = int(round(w * scale))
-
-        resized_image = tf.image.resize(
-            image,
+        image_scaled = image / 255.0
+        image_resized = tf.image.resize(
+            image_scaled,
             size=[h_new, w_new],
             method='bicubic'
         )
+        image_resized = tf.clip_by_value(image_resized, 0.0, 1.0)
 
-        rescaled_image = resized_image / 255.0
-        rescaled_image = tf.clip_by_value(rescaled_image, 0.0, 1.0)
-
-        return tf.expand_dims(rescaled_image, axis=0)
+        return tf.expand_dims(image_resized, axis=0)
